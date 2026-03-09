a8656a6546040f20cb28b8f43844ced8778accbe
Tobi <tobiomonona1@gmail.com>
2026-03-09 17:59:43 +0100

    Added max attempts and input validation to guessing game

--- guessgame.py
+++ guessgame.py
@@ -3,11 +3,20 @@ import random
 secret_number = random.randint(1, 100)
 
 attempts = 0
+max_attempts = 7
 
-while True:
+print("Welcome to the Guessing Game!")
+print("You have", max_attempts, "attempts to guess the number.")
+
+while attempts < max_attempts:
+
     guess = int(input("Guess a number between 1 and 100: "))
     
     attempts += 1
+
+    if guess < 1 or guess > 100:
+        print("Please enter a number between 1 and 100")
+        continue
 
     if guess > secret_number:
         print("Too high")
@@ -19,3 +28,7 @@ while True:
         print("Correct!")
         print("You guessed the number in", attempts, "attempts")
         break
+
+if guess != secret_number:
+    print("Game Over!")
+    print("The secret number was:", secret_number)
